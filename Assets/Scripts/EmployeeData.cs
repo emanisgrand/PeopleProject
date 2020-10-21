71bf7a85bbc4245f2c23244485938d8fc0e3f247
emanisgrand <tenhitcombo@gmail.com>
2020-10-21 15:24:14 -0400

    initial commit

---
+++ Assets/Scripts/EmployeeData.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EmployeeData : ScriptableObject {
+    public enum EXPERIENCE_LEVEL { 
+        unknown, 
+        Junior,
+        Lead,
+        Senior
+    }
+
+    [SerializeField] string _employeeName;
+    public string EmployeeName => _employeeName;
+    
+    [SerializeField] EXPERIENCE_LEVEL _experienceLevelLevel;
+    public EXPERIENCE_LEVEL ExperienceLevel => _experienceLevelLevel;
+    
+    // presenting
+    [SerializeField] Texture _icon;
+    public Texture Icon => _icon;
+
+
+}
