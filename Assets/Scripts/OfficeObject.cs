dd17c7faec4cce940bb82727eb20b5417ce7e594
emanisgrand <tenhitcombo@gmail.com>
2020-11-12 16:40:41 -0500

    added a few packages for UI and Astar. Added AI logic

--- Assets/Scripts/OfficeObject.cs
+++ Assets/Scripts/OfficeObject.cs
@@ -8,11 +8,17 @@ public class OfficeObject : MonoBehaviour
    float outlineSize = 0.07f;
    float defaultOutline = 0;
    
+   
    private void Awake()
    {
       rend = GetComponent<Renderer>();
    }
-   
+
+   private void Start()
+   {
+      rend.material.SetFloat("_OutlineThickness", defaultOutline);
+   }
+
    private void OnMouseEnter()
    {
       rend.material.SetFloat("_OutlineThickness", outlineSize);
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,7 +5,7 @@ using UnityEngine.AI;
 
 public class Player : MonoBehaviour
 {
-    
+    public float Money;
     NavMeshAgent agent;
     
     private void Awake()
