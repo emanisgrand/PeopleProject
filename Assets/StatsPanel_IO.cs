bb085eba6d00431506766b740bef5323a867b637
emanisgrand <tenhitcombo@gmail.com>
2020-12-03 17:28:49 -0500

    skill tree UI and office objects

---
+++ Assets/StatsPanel_IO.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StatsPanel_IO : MonoBehaviour
+{
+    public GameObject skillTreePanel;
+    private bool isPanelClosed;
+
+    private void Awake()
+    {
+        skillTreePanel = GetComponent<RectTransform>().gameObject;
+    }
+
+    public void OpenSkillTreePanel()
+    {
+        if (isPanelClosed)
+        skillTreePanel.SetActive(true);
+        isPanelClosed = false;
+    }
+
+    public void CloseSkillTreePanel()
+    {
+        if (isPanelClosed)
+        {
+            skillTreePanel.SetActive(false);
+        }
+    }
+    
+}
